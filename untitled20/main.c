b601d5de29d90ca4a42a906e82117a0cd6eadac5
jooodie <jodiechu1204@gmail.com>
2021-12-01 21:53:09 +0800

    交換三個變數值

--- untitled20/main.c
+++ untitled20/main.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
-
-int a,b,c, output;
+//交換三個變數質//
+int a,b,c,temp;
 int main(){
-    a=2;
-    b=4;
-    c=6 ;
-    output = a + (b*c);
-    printf("%d", output);
-
-    return 0;
+    a = 4; b = 5; c = 6;
+    printf("%d\n%d\n%d\n\n", a, b, c);
+    temp = a;
+    a = b;
+    b = c;
+    c = temp;
+    printf("%d\n", a);
+    printf("%d\n", b);
+    printf("%d\n", c);
+    return 0 ;
 }
